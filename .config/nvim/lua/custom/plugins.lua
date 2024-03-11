420c696b80822f4ca305a20570fa66bdc1f71941
Surya Pranav Sukumaran <ps2pranav@gmail.com>
2024-03-11 07:43:59 -0400

    nvim config

---
+++ .config/nvim/lua/custom/plugins.lua
@@ -0,0 +1,176 @@
+local plugins = {
+  {
+    "rcarriga/nvim-dap-ui",
+    dependencies = "mfussenegger/nvim-dap",
+    config = function()
+      local dap = require("dap")
+      local dapui = require("dapui")
+      dapui.setup()
+      dap.listeners.after.event_initialized["dapui_config"] = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated["dapui_config"] = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited["dapui_config"] = function()
+        dapui.close()
+      end
+    end,
+  },
+  {
+    "mfussenegger/nvim-dap",
+    config = function(_, opts)
+      require("core.utils").load_mappings("dap")
+    end,
+  },
+  {
+    "mfussenegger/nvim-dap-python",
+    ft = "python",
+    dependencies = {
+      "mfussenegger/nvim-dap",
+      "rcarriga/nvim-dap-ui",
+    },
+    config = function(_, opts)
+      local path = "~/.local/share/nvim/mason/packages/debugpy/venv/bin/python"
+      require("dap-python").setup(path)
+      require("core.utils").load_mappings("dap_python")
+    end,
+  },
+  {
+    "nvimtools/none-ls.nvim",
+    ft = {"python"},
+    opts = function()
+      return require "custom.configs.null-ls"
+    end,
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "black",
+        "debugpy",
+        "mypy",
+        "ruff",
+        "pyright",
+        "eslint_d",
+      },
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end,
+  },
+
+  {
+    "christoomey/vim-tmux-navigator", lazy = false,
+    event = "BufReadPre",
+  },
+   {
+        'goolord/alpha-nvim',
+        lazy = true,
+        event = "VimEnter",
+        config = function ()
+            -- Define the configure function
+            local function configure()
+                local dashboard = require('alpha.themes.dashboard')
+
+                -- Custom settings for the dashboard
+                vim.api.nvim_set_hl(0, "NeovimDashboardLogo1", { fg = "#0D47A1" }) -- Dark Blue
+                vim.api.nvim_set_hl(0, "NeovimDashboardLogo2", { fg = "#1976D2" }) -- Medium Blue
+                vim.api.nvim_set_hl(0, "NeovimDashboardLogo3", { fg = "#64B5F6" }) -- Light Blue
+                vim.api.nvim_set_hl(0, "NeovimDashboardLogo4", { fg = "#424242" }) -- Dark Gray
+                vim.api.nvim_set_hl(0, "NeovimDashboardLogo5", { fg = "#757575" }) -- Medium Gray
+                vim.api.nvim_set_hl(0, "NeovimDashboardLogo6", { fg = "#BDBDBD" }) -- Light Gray
+
+                dashboard.section.header.type = "group"
+                dashboard.section.header.val = {
+                    
+        { type = "text", val = "   ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ", opts = { hl = "NeovimDashboardLogo1", shrink_margin = false, position = "center" } },
+        { type = "text", val = "   ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ", opts = { hl = "NeovimDashboardLogo2", shrink_margin = false, position = "center" } },
+        { type = "text", val = "   ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ", opts = { hl = "NeovimDashboardLogo3", shrink_margin = false, position = "center" } },
+        { type = "text", val = "   ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ", opts = { hl = "NeovimDashboardLogo4", shrink_margin = false, position = "center" } },
+        { type = "text", val = "   ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ", opts = { hl = "NeovimDashboardLogo5", shrink_margin = false, position = "center" } },
+        { type = "text", val = "   ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ", opts = { hl = "NeovimDashboardLogo6", shrink_margin = false, position = "center" } },
+
+                    -- Header customization
+                }
+
+                dashboard.section.buttons.val = {
+
+        dashboard.button("n", " " .. " New file", ":ene <BAR> startinsert <CR>"),
+        dashboard.button("f", " " .. " Find file", ":Telescope find_files <CR>"),
+        dashboard.button("g", "󰷾 " .. " Find text", ":Telescope live_grep <CR>"),
+        dashboard.button("b", " " .. " File browser", ":NvimTreeFocus <CR>"),
+        dashboard.button("r", "󰄉 " .. " Recent files", ":Telescope oldfiles <CR>"),
+        dashboard.button("c", " " .. " Config", ":e ~/.config/nvim/lua/custom/plugins.lua <CR>"),
+        dashboard.button("l", "󰒲 " .. " Lazy", ":Lazy<CR>"),
+        dashboard.button("q", " " .. " Quit", ":qa<CR>"),
+                    -- Buttons customization
+                }
+
+                return dashboard.config
+            end
+
+            -- Setup alpha with the configure function
+            require('alpha').setup(configure())
+        end,
+    },
+  {
+     "nvim-neorg/neorg",
+    build = ":Neorg sync-parsers",
+    lazy = false, -- specify lazy = false because some lazy.nvim distributions set lazy = true by default
+    -- tag = "*",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("neorg").setup {
+        load = {
+          ["core.defaults"] = {}, -- Loads default behaviour
+          ["core.concealer"] = {}, -- Adds pretty icons to your documents
+          ["core.dirman"] = { -- Manages Neorg workspaces
+            config = {
+              workspaces = {
+                notes = "~/notes",
+              },
+            },
+          },
+        },
+      }
+    end,
+  },
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    build = function() vim.fn["mkdp#util#install"]() end,
+  },
+  {
+    "jpalardy/vim-slime", lazy=false
+  },
+  {
+	"michaelrommel/nvim-silicon",
+	lazy = true,
+	cmd = "Silicon",
+  init = function()
+      local wk = require("which-key")
+      wk.register({
+        ["<leader>sc"] = { ":Silicon<CR>", "Snapshot Code" },
+      }, { mode = "v" })
+  end,
+	config = function()
+		require("silicon").setup({
+			-- Configuration here, or leave empty to use defaults
+			font = "VictorMono NF=34;Noto Emoji=34",
+      theme = "Dracula",
+      background = "#fff"
+		})
+	end
+},
+}
+return plugins
+
+
+
+
